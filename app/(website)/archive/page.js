2a4e2fef5873c3462590e92939f5833999c3cf28
Carlo Kruger <ironicbuddha@gmail.com>
2025-08-26 08:55:16 +0200

    Remove edge runtime from archive page

--- app/(website)/archive/page.js
+++ app/(website)/archive/page.js
@@ -5,7 +5,7 @@ import Loading from "@/components/loading";
 
 export const dynamic = "force-dynamic";
 
-export const runtime = "edge";
+// export const runtime = "edge";
 
 export default async function ArchivePage({ searchParams }) {
   return (
@@ -29,4 +29,4 @@ export default async function ArchivePage({ searchParams }) {
   );
 }
 
-// export const revalidate = 60;
+export const revalidate = 60;
